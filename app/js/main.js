d61709ae3bc48ecfaf27085a1cea99ccc7ceff92
Alexeykormiltsev <cormiltsev@yandex.ru>
2023-05-13 20:13:59 +0500

    k

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,7 @@
-$(function(){
-
+$(function () {
+    $('.menu-btn').on('click', function () {
+        $('.menu').toggleClass('menu--active');
+        $('.menu-btn').toggleClass('menu-btn--active');
+        // $('body').toggleClass('hidden');
+    });
 });
